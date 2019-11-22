c837ffa817698a09c2167f6cdc751faf39103de0
Alexander Babai <aliaksandr.babai@gmail.com>
2019-11-22 18:19:25 +0300

    feat(filter): add styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
 import classNames from 'classnames'
 
+import styles from './style.css'
+
 const FilterTab = ({ id, title, active, handler }) => (
-    <div key={id} className={classNames('filter-tab', { 'filter-tab-active': active })} onClick={() => handler(id)}>{title || id}</div>
+    <div key={id} className={classNames(styles.tab, { [styles.tabActive]: active })} onClick={() => handler(id)}>{title || id}</div>
 )
 
 export const Filter = ({ title, defaultActiveTab, tabs = [] }) => {
     const [activeTab, setState] = useState(defaultActiveTab)
     const handler = id => setState(id)
     return (
-        <div className="filter">
-            <p className="filter-title">{title}</p>
-            <div className="filter-tabs">
-                {tabs.map(tab => ({ ...tab, handler, active: activeTab === tab.id })).map(FilterTab)}
+        <div className={styles.root}>
+            <p className={styles.title}>{title}</p>
+            <div className={styles.tabs}>
+                {tabs.map(({ id, title }) => ({ id, title, handler, active: activeTab === id })).map(FilterTab)}
             </div>
         </div>
     )
