32a32c674acac4a7b677c99df481dc51bab66ada
Alexander Babai <aliaksandr.babai@gmail.com>
2019-12-02 17:18:59 +0300

    test(store): cover actions

---
+++ src/store/actions/data.spec.js
@@ -0,0 +1,50 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import { loadOne, loadMany } from './data'
+
+const data = [{ id: '1', genres: [] }, { id: '2', genres: [] }, { id: '3', genres: [] }]
+const apiMock = {
+    getOne: id => Promise.resolve(data.find(item => item.id === id)),
+    getMany: () => Promise.resolve({ items: data, offset: 0, limit: 10, total: data.length }),
+}
+const mockStore = configureMockStore([
+    thunk.withExtraArgument({ api: apiMock })
+])
+
+let store
+
+beforeEach(() => {
+    store = mockStore({
+        search: {
+            query: '',
+            searchBy: 'title',
+            active: false
+        },
+        fetch: {
+            sortBy: 'release_date',
+            sortOrder: 'asc',
+            filter: undefined,
+            offset: undefined,
+            limit: undefined,
+        },
+        data: {
+            item: {
+                genres: [],
+            },
+            items: [],
+        },
+    })
+})
+
+test('Should create bunch of async actions to load one item', () => {
+    store.dispatch(loadOne(data[0].id)).then(() => {
+        const actions = store.getActions()
+        expect(actions[0].type).toEqual('@DATA__REQUEST')
+        expect(actions[1].type).toEqual('@DATA__LOADED_ONE')
+        expect(actions[2].type).toEqual('@FETCH__UPDATE_PARAMS')
+        expect(actions[3].type).toEqual('@DATA__REQUEST')
+        expect(actions[4].type).toEqual('@DATA__LOADED_MANY')
+        expect(actions[5].type).toEqual('@FETCH__UPDATE_PARAMS')
+    })
+})
---
+++ src/store/actions/search.spec.js
@@ -0,0 +1,21 @@
+import { updateSearchParams, resetSearchParams } from './search'
+
+test('Should create action to update params', () => {
+    const params = {
+        query: 'Fight CLub',
+        searchBy: 'title',
+        active: false
+    }
+    const expectedAction = {
+        type: '@SEARCH__UPDATE_PARAMS',
+        payload: params,
+    }
+    expect(updateSearchParams(params)).toEqual(expectedAction)
+})
+
+test('Should create action to reset params', () => {
+    const expectedAction = {
+        type: '@SEARCH__RESET_PARAMS',
+    }
+    expect(resetSearchParams()).toEqual(expectedAction)
+})
