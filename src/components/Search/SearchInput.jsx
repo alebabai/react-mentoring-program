609a9f85260ae1fc4d3501d710a06ea04c2370ef
Alexander Babai <aliaksandr.babai@gmail.com>
2019-12-04 15:43:27 +0300

    refactor: use value instead of defaultValue

--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './styles.css'
 
 export const SearchInput = ({ placeholder, value, onChange, onEnterKeyDown }) => (
-    <input onKeyDown={e => e.key === 'Enter' && onEnterKeyDown()} onChange={e => onChange(e.currentTarget.value)} className={styles.searchInput} placeholder={placeholder} defaultValue={value} />
+    <input onKeyDown={e => e.key === 'Enter' && onEnterKeyDown()} onChange={e => onChange(e.currentTarget.value)} className={styles.searchInput} placeholder={placeholder} value={value} />
 )
 
 export default SearchInput
