1e2323677f7daffb6f316d1d44e0f090649eba65
Alexander Babai <aliaksandr.babai@gmail.com>
2019-12-10 15:10:44 +0300

    refactor(components): rename style var

--- src/components/Search/SearchButton.jsx
+++ src/components/Search/SearchButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import styles from './styles.css'
+import style from './style.css'
 
 export const SearchButton = ({ text }) => (
-    <button type="submit" className={styles.searchButton}>
-        <p className={styles.searchButtonText}>{text}</p>
+    <button type="submit" className={style.searchButton}>
+        <p className={style.searchButtonText}>{text}</p>
     </button>
 )
 
--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import styles from './styles.css'
+import style from './style.css'
 
 export const SearchInput = ({ placeholder, value, onChange }) => (
-    <input onChange={e => onChange(e.currentTarget.value)} className={styles.searchInput} placeholder={placeholder} value={value} />
+    <input onChange={e => onChange(e.currentTarget.value)} className={style.searchInput} placeholder={placeholder} value={value} />
 )
 
 export default SearchInput
