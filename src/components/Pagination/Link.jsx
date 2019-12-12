a8d9bc9bf5ec193e9c970ca174bba1cdf2b2a664
Alexander Babai <aliaksandr.babai@gmail.com>
2019-12-12 15:55:52 +0300

    refactor(pagination): fix button type

--- src/components/Pagination/Link.jsx
+++ src/components/Pagination/Link.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames/bind'
 import style from './style.css'
 
 export const Link = ({ value, text = value, active, onClick }) => (
-    <button className={classNames(style.link, { [style.linkActive]: active })} onClick={() => !active && onClick(value)}>{text}</button>
+    <button type="button" className={classNames(style.link, { [style.linkActive]: active })} onClick={() => !active && onClick(value)}>{text}</button>
 )
 
 export default Link
